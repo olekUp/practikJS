d09e7f1e10469429c4cd1daa77d527d591fd5013
Up <sashkaolichyk94@gmail.com>
2023-05-17 18:52:38 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/* alert("Hello");
+
+ */
+
+/* const result = confirm("Are you sure you want to?");
+console.log(result); */
+
+/* const answers = prompt("are you 18 ears old?", "yes");
+console.log(answers);
+
+ */
+
+const answers = [];
+
+answers[0] = prompt("what you name?", "");
+answers[1] = prompt("what you surname?", "");
+answers[2] = prompt("how old are you?", "");
+
+document.write(answers);
